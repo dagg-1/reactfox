feae5de876943f19becc955b11e5d27d1a7590d4
Dagg <daggintosh@outlook.com>
2019-07-29 13:27:44 -0700

    YAH

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ function App() {
   return (
     <React.Fragment>
         <div className="App">
-        <img src="https://dagg.xyz/randomfox/images/91.jpg" alt="Fox!" className="FoxImage" />
+        <img src={"https://dagg.xyz/randomfox/images/" + (Math.floor(Math.random() * 124) + 1) + ".jpg"} alt="Fox!" className="FoxImage" />
         <div className="RefreshBar">
-          <img src={foxface} alt="Button" className="Button" />
+          <img src={foxface} alt="Button" className="Button" onClick={reload} />
         </div>
       </div>
     </React.Fragment>
   );
+}
+
+function reload()
+{
+  setTimeout(z => { window.location.reload() }, 1000)
 }
 
 export default App;
