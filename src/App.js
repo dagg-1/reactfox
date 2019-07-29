233b8ebeac008e3eb922206479f8b7734f90323b
Dagg <daggintosh@outlook.com>
2019-07-29 07:41:22 -0700

    Structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import foxface from './foxface.png';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello, World</p>
-    </div>
+    <React.Fragment>
+        <div className="App">
+        <img src="https://dagg.xyz/randomfox/images/91.jpg" alt="Fox!" className="FoxImage" />
+        <div className="RefreshBar">
+          <img src={foxface} alt="Button" className="Button" />
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
